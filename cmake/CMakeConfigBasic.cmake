8457fdaa5f8de8ccace8b0e2dca6d554aa4e247a
en2 <ernest.szczepaniak.en2@gmail.com>
2020-05-11 20:32:28 +0200

    push

--- cmake/CMakeConfigBasic.cmake
+++ cmake/CMakeConfigBasic.cmake
@@ -8,8 +8,8 @@ set(version_major               1)
 set(version_minor               0)
 set(version_revision            0)
 
-option(dump ""                  on)
-option(image ""                 on)
+option(dump ""                  off)
+option(image ""                 off)
 option(documentation ""         off)
 
 # ---------------------------------------------| project |--------------------------------------------- #
