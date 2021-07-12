f4ad064ef6e29cd12afff157d166bbb829c8c696
en2 <ernest.szczepaniak.en2@gmail.com>
2021-07-12 09:03:59 +0200

    poprzednie testy pamieci spi

--- main.cpp
+++ main.cpp
@@ -20,66 +20,26 @@
 // SS1 D21 brazowy
 
 
-void write_enable(int slave)
-{
-    unsigned char tx = 0x06;
-
-    h_spi_write(&tx, 1, 0);
-}
-
-void write(int address, unsigned char * buffer, int size, int slave)
-{
-    unsigned char temp[1024];
-
-    temp[0] = 0x02;
-    temp[1] = (address >> 16) & 0xff;
-    temp[2] = (address >> 8) & 0xff;
-    temp[3] = (address >> 0) & 0xff;
-    memcpy(&temp[4], buffer, size);
-
-    h_spi_write(temp, size + 4, slave);
-}
-
-void read(int address, unsigned char * buffer, int size, int slave)
-{
-    unsigned char temp[1024];
-
-    temp[0] = 0x03;
-    temp[1] = (address >> 16) & 0xff;
-    temp[2] = (address >> 8) & 0xff;
-    temp[3] = (address >> 0) & 0xff;
-
-    h_spi_write_read(temp, 4, buffer, size, slave);
-}
-
-void delay()
-{
-    for (int i = 0;i < 10000; i++);
-}
-
 int main()
 {
-    h_spi_init(1e3);
+    auto value = h_sd_init();
 
-    write_enable(1);
-    unsigned char tx[16];
-    unsigned char rx[16];
-    
+    auto size = 512;
 
-    for (int i = 0; i < 16; i++)
+    unsigned char tx[size];
+    unsigned char rx[size] = {0};
+
+    for (int i = 0; i < 512; i++)
     {
         tx[i] = i;
     }
-    
-    while(1)
-    {
 
-        write(0x10, tx, 5, 1);
+    auto status = h_sd_read(0, rx, size);
+    status = h_sd_write(0, tx, size);
+    status = h_sd_read(0, rx, size);
 
-        read(0x12, rx, 5, 1);
 
-        delay();
-    }
+    while(1);
 }
 
 #endif
